becdb2427ca2a5396e2e60f1ac624f228c0a0925
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2019-10-16 13:20:32 -0700

    formatting

--- js.js
+++ js.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
-   
+$(document).ready(function () {
+
     //GLOBAL VARIABLES
     var gameCounter = 0;
     var wins = 0;
     var losses = 0;
     var numberToGuess = 0;
-    
+
     const crystalOne = {
-       button: $('#button-left'),
-       value:  0
+        button: $('#button-left'),
+        value: 0
     };
     const crystalTwo = {
         button: $('#button-middle'),
@@ -21,67 +21,67 @@ $(document).ready(function() {
 
 
     //FUNCTIONS
-   function startGame() {
-    numberToGuess = Math.floor((Math.random() * 121) + 19);
-    crystalOne.value = Math.floor((Math.random() * 13) + 1);
-    crystalTwo.value = Math.floor((Math.random() * 13) + 1);
-    crystalThree.value = Math.floor((Math.random() * 13) + 1);
-    gameCounter = 0;
-    console.log(crystalOne.value);
-    console.log(crystalTwo.value);
-    console.log(crystalThree.value);
-    console.log('Number to Guess: ' + numberToGuess);
-   }
+    function startGame() {
+        numberToGuess = Math.floor((Math.random() * 121) + 19);
+        crystalOne.value = Math.floor((Math.random() * 13) + 1);
+        crystalTwo.value = Math.floor((Math.random() * 13) + 1);
+        crystalThree.value = Math.floor((Math.random() * 13) + 1);
+        gameCounter = 0;
+        console.log(crystalOne.value);
+        console.log(crystalTwo.value);
+        console.log(crystalThree.value);
+        console.log('Number to Guess: ' + numberToGuess);
+    }
 
-   function counter() {
-    $(crystalOne.button).on('click', function() {
-        gameCounter = gameCounter + crystalOne.value;
-        console.log('Game Counter: ' + gameCounter)
+    function counter() {
+        $(crystalOne.button).on('click', function () {
+            gameCounter = gameCounter + crystalOne.value;
+            console.log('Game Counter: ' + gameCounter)
 
-        if (gameCounter > numberToGuess) {
-            losses ++ 
-            console.log('Losses: ' + losses);
-            startGame()
-        }
-        else if (gameCounter === numberToGuess) {
-            wins ++
-            console.log('Wins: ' + wins);
-            startGame();
-        }
-    });
-    $(crystalTwo.button).on('click', function() {
-        gameCounter = gameCounter + crystalTwo.value;
-        console.log('Game Counter: ' + gameCounter)
-        if (gameCounter > numberToGuess) {
-            losses ++ 
-            console.log('Losses: ' + losses);
-            startGame()
-        }
-        else if (gameCounter === numberToGuess) {
-            wins ++
-            console.log('Wins: ' + wins);
-            startGame();
-        }
-    });
-    $(crystalThree.button).on('click', function() {
-        gameCounter = gameCounter + crystalThree.value;
-        console.log('Game Counter: ' + gameCounter)
-        if (gameCounter > numberToGuess) {
-            losses ++ 
-            console.log('Losses: ' + losses);
-            startGame()
-        }
-        else if (gameCounter === numberToGuess) {
-            wins ++
-            console.log('Wins: ' + wins);
-            startGame();
-        }
-    });
-    
-   }
+            if (gameCounter > numberToGuess) {
+                losses++
+                console.log('Losses: ' + losses);
+                startGame()
+            }
+            else if (gameCounter === numberToGuess) {
+                wins++
+                console.log('Wins: ' + wins);
+                startGame();
+            }
+        });
+        $(crystalTwo.button).on('click', function () {
+            gameCounter = gameCounter + crystalTwo.value;
+            console.log('Game Counter: ' + gameCounter)
+            if (gameCounter > numberToGuess) {
+                losses++
+                console.log('Losses: ' + losses);
+                startGame()
+            }
+            else if (gameCounter === numberToGuess) {
+                wins++
+                console.log('Wins: ' + wins);
+                startGame();
+            }
+        });
+        $(crystalThree.button).on('click', function () {
+            gameCounter = gameCounter + crystalThree.value;
+            console.log('Game Counter: ' + gameCounter)
+            if (gameCounter > numberToGuess) {
+                losses++
+                console.log('Losses: ' + losses);
+                startGame()
+            }
+            else if (gameCounter === numberToGuess) {
+                wins++
+                console.log('Wins: ' + wins);
+                startGame();
+            }
+        });
+
+    }
 
     //GAME FUNCTIONALITY
     startGame();
     counter();
-  
+
 })
